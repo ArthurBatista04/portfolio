c3291e8a2070563c39bc83d3fae327907aabe875
arthurb2009_00 <arthur@eurekalabs.com.br>
2020-05-06 22:26:54 -0300

    update routes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { ADMIN, MAIN } from "./Routes";
+import { MAIN } from "./Routes";
 import { config } from "./Firebase";
 import { emailRegEx } from "./Regex";
-export { ADMIN, MAIN, config, emailRegEx };
+export { MAIN, config, emailRegEx };
